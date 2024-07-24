753eae275742e7ad700774c3d23dd10052434d05
Andrew Toader <69984232+aet37@users.noreply.github.com>
2024-07-24 13:14:29 -0700

    commit

--- PlotFunctions.py
+++ PlotFunctions.py
@@ -109,6 +109,7 @@ def analyze_accel_data(t_pa, x_pa, y_pa, z_pa):
 	# Get P-P Amplitude
 	max_pks = find_peaks(accel_data_filt)
 	tst, _ = find_peaks(np.negative(accel_data_filt))
+	print(tst)
 	print(find_peaks(np.negative(accel_data_filt)))
 	min_pks = np.negative(find_peaks(np.negative(accel_data_filt)))
 	peak_peak = round(np.mean(max_pks - min_pks), 5)
