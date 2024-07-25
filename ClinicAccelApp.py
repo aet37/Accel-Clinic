2e927826688d5bdb9967de74be2a9d3fa4fd6c04
Andrew Toader <69984232+aet37@users.noreply.github.com>
2024-07-25 16:15:15 -0700

    commit

--- ClinicAccelApp.py
+++ ClinicAccelApp.py
@@ -378,7 +378,7 @@ class spiralDrawSystem(QtWidgets.QMainWindow):
 			c.drawString(50, height - 170, 'AUC of PSD (4-12Hz): ' + str(display_statistics[1]) + ' G*Hz')
 
 			# Add the PSD figure in
-			c.drawImage(ImageReader(self.data_save_path + 'analysis/pdf_figs/' + self.accel_psds[i] + '_psd.png'), 50, height - 190, width=400, preserveAspectRatio=True, mask='auto')
+			c.drawImage(ImageReader(self.data_save_path + 'analysis/pdf_figs/' + self.accel_psds[i] + '_psd.png'), 50, height - 50, width=400, preserveAspectRatio=True, mask='auto')
 
 
 			# Plot CCW spiral if it exists
